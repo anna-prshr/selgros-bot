2043caee0972b8e738d454229504934809f9e96a
anna-prshr <anna_prescher@icloud.com>
2026-02-04 08:01:28 +0100

    Update selgros.ts

--- src/selgros.ts
+++ src/selgros.ts
@@ -1,1 +1,10 @@
+export async function runSelgrosSearch(keywords: string[]) {
+  console.log("Selgros search called with:", keywords);
 
+  // Platzhalter-Ergebnis (damit alles technisch funktioniert)
+  return keywords.map((keyword) => ({
+    keyword,
+    products: [],
+    note: "Selgros Bot ist verbunden (Platzhalter)"
+  }));
+}
